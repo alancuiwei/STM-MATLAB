714572007119a9be306450a2e2191ecf515aee2f
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-07-10 17:52:58 +0800

    29610543

--- StrategyProcess/Strategies/ZR_STRATEGY_040706.m
+++ StrategyProcess/Strategies/ZR_STRATEGY_040706.m
@@ -1,4 +1,4 @@
-function outputdata=ZR_STRATEGY_040706(inputdata)
+function [outputdata,TradeDay]=ZR_STRATEGY_040706_new(inputdata)
 %tmp=load('inRO.mat');
 %inputdata=tmp.l_inputdata;
 %==========================================================================
@@ -105,66 +105,66 @@ for i=1:numel(RealTradeDay)
 end
 %==========================================================================
 %第一步矫正record,修改强制平仓价格以及当天的开仓价格
-for i=1:numel(ForceTrade)
-    PositionForceInTrade=find(RealTradeDay==ForceTrade(i));
-    if (PositionForceInTrade>1)
-        if(ForceTrade(i)+2>numel(inputdata.commodity.serialmkdata.date)) %假如交点为今天和昨天之间，则更新outputdata.orderlist向量
-                outputdata.orderlist.direction=1;
-                outputdata.orderlist.price=0;
-        else
-            outputdata.record.direction(PositionForceInTrade)=outputdata.record.direction(PositionForceInTrade-1);
-            outputdata.record.opdateprice(PositionForceInTrade)=inputdata.commodity.serialmkdata.op(ForceTrade(i)+2)+inputdata.commodity.serialmkdata.gap(ForceTrade(i)+2);
-            outputdata.record.opdate(PositionForceInTrade)=inputdata.commodity.serialmkdata.date(ForceTrade(i)+2);
-        end
-    end
-end
+% for i=1:numel(ForceTrade)
+%     PositionForceInTrade=find(RealTradeDay==ForceTrade(i));
+%     if (PositionForceInTrade>1)
+%         if(ForceTrade(i)+2>numel(inputdata.commodity.serialmkdata.date)) %假如交点为今天和昨天之间，则更新outputdata.orderlist向量
+%                 outputdata.orderlist.direction=1;
+%                 outputdata.orderlist.price=0;
+%         else
+%             outputdata.record.direction(PositionForceInTrade)=outputdata.record.direction(PositionForceInTrade-1);
+%             outputdata.record.opdateprice(PositionForceInTrade)=inputdata.commodity.serialmkdata.op(ForceTrade(i)+2)+inputdata.commodity.serialmkdata.gap(ForceTrade(i)+2);
+%             outputdata.record.opdate(PositionForceInTrade)=inputdata.commodity.serialmkdata.date(ForceTrade(i)+2);
+%         end
+%     end
+% end
 %==========================================================================
 
 %完善outputdata.record,填入平仓日期和平仓价格
-if(numel(outputdata.record.opdate)>=2)
-    outputdata.record.cpdate=outputdata.record.opdate(2:end);
-    outputdata.record.cpdateprice=outputdata.record.opdateprice(2:end);
-    outputdata.record.isclosepos=ones(1,numel(outputdata.record.opdateprice)-1);
-    outputdata.record.isclosepos(numel(outputdata.record.opdateprice))=0;
-    outputdata.record.cpdate(numel(outputdata.record.opdateprice))=inputdata.commodity.serialmkdata.date(end); 
-    outputdata.record.cpdateprice(numel(outputdata.record.opdateprice))=inputdata.commodity.serialmkdata.op(end);
-    %{
-    if(inputdata.contract.info.daystolasttradedate<=0) 
-        outputdata.record.isclosepos(numel(outputdata.record.opdateprice))=1;
-    end
-    %}
-elseif(numel(outputdata.record.opdate)>=1)
-    outputdata.record.cpdate=inputdata.commodity.serialmkdata.date(end);
-    outputdata.record.cpdateprice=inputdata.commodity.serialmkdata.op(end)+inputdata.commodity.serialmkdata.gap(end);
-    outputdata.record.isclosepos=0;
-    %{
-    if(inputdata.contract.info.daystolasttradedate<=0) 
-        outputdata.record.isclosepos(numel(outputdata.record.opdateprice))=1;
-    end
-    %}
-end
+% if(numel(outputdata.record.opdate)>=2)
+%     outputdata.record.cpdate=outputdata.record.opdate(2:end);
+%     outputdata.record.cpdateprice=outputdata.record.opdateprice(2:end);
+%     outputdata.record.isclosepos=ones(1,numel(outputdata.record.opdateprice)-1);
+%     outputdata.record.isclosepos(numel(outputdata.record.opdateprice))=0;
+%     outputdata.record.cpdate(numel(outputdata.record.opdateprice))=inputdata.commodity.serialmkdata.date(end); 
+%     outputdata.record.cpdateprice(numel(outputdata.record.opdateprice))=inputdata.commodity.serialmkdata.op(end);
+%     %{
+%     if(inputdata.contract.info.daystolasttradedate<=0) 
+%         outputdata.record.isclosepos(numel(outputdata.record.opdateprice))=1;
+%     end
+%     %}
+% elseif(numel(outputdata.record.opdate)>=1)
+%     outputdata.record.cpdate=inputdata.commodity.serialmkdata.date(end);
+%     outputdata.record.cpdateprice=inputdata.commodity.serialmkdata.op(end)+inputdata.commodity.serialmkdata.gap(end);
+%     outputdata.record.isclosepos=0;
+%     %{
+%     if(inputdata.contract.info.daystolasttradedate<=0) 
+%         outputdata.record.isclosepos(numel(outputdata.record.opdateprice))=1;
+%     end
+%     %}
+% end
 %==========================================================================
 %第二步矫正record,修改强制平仓价格以及当天的开仓价格
-for i=1:numel(ForceTrade)
-    PositionForceInTrade=find(RealTradeDay==ForceTrade(i));
-    if (PositionForceInTrade>1) 
-        %修改于移仓操作相关的平仓价格，需要上一步的合约名(cntname)，以及移仓日期(ForceTrade+2)
-        %合约名ID的生成来源于cntname以及合约的日期
-        if(ForceTrade(i)+2>numel(inputdata.commodity.serialmkdata.date)) %假如交点为今天和昨天之间，则更新outputdata.orderlist向量
-                outputdata.orderlist.direction=1;
-                outputdata.orderlist.price=0;
-        else
-            CntName=inputdata.commodity.serialmkdata.ctname(ForceTrade(i));
-            CntName;
-            DateNum=inputdata.commodity.serialmkdata.date(ForceTrade(i)+2);
-            DateNum;
-            CntID=find(ismember(inputdata.contractname,CntName)==1);
-            find(ismember(inputdata.contract(1,CntID).mkdata.date,DateNum)==1);
-            DataID=find(ismember(inputdata.contract(1,CntID).mkdata.date,DateNum)==1);
-            outputdata.record.cpdateprice(PositionForceInTrade-1)=inputdata.contract(1,CntID).mkdata.op(DataID);
-        end
-    end
-end
+% for i=1:numel(ForceTrade)
+%     PositionForceInTrade=find(RealTradeDay==ForceTrade(i));
+%     if (PositionForceInTrade>1) 
+%         %修改于移仓操作相关的平仓价格，需要上一步的合约名(cntname)，以及移仓日期(ForceTrade+2)
+%         %合约名ID的生成来源于cntname以及合约的日期
+%         if(ForceTrade(i)+2>numel(inputdata.commodity.serialmkdata.date)) %假如交点为今天和昨天之间，则更新outputdata.orderlist向量
+%                 outputdata.orderlist.direction=1;
+%                 outputdata.orderlist.price=0;
+%         else
+%             CntName=inputdata.commodity.serialmkdata.ctname(ForceTrade(i));
+%             CntName;
+%             DateNum=inputdata.commodity.serialmkdata.date(ForceTrade(i)+2);
+%             DateNum;
+%             CntID=find(ismember(inputdata.contractname,CntName)==1);
+%             find(ismember(inputdata.contract(1,CntID).mkdata.date,DateNum)==1);
+%             DataID=find(ismember(inputdata.contract(1,CntID).mkdata.date,DateNum)==1);
+%             outputdata.record.cpdateprice(PositionForceInTrade-1)=inputdata.contract(1,CntID).mkdata.op(DataID);
+%         end
+%     end
+% end
 % figure('Name',cell2mat(inputdata.commodity.name));
 % plot(Price(1,:),'b');
 % hold on;
