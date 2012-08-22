0f4d8629e74a06ee80823b7c6f150392ff1ca5be
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-08-22 15:06:36 +0800

    040714 bug fix

--- StrategyProcess/Strategies/ZR_STRATEGY_040714.m
+++ StrategyProcess/Strategies/ZR_STRATEGY_040714.m
@@ -26,14 +26,15 @@ l_hp = inputdata.commodity.serialmkdata.hp;
 l_lp = inputdata.commodity.serialmkdata.lp;
 
 
-[l_AroonUp,l_AroonDown] = TA_AROON(l_lp, l_hp, inputdata.strategyparams.period);
+[l_AroonUp,l_AroonDown] = TA_AROON(l_hp, l_lp, inputdata.strategyparams.period);
 
-
-plot(l_AroonUp, '-b');
-hold on
-plot(l_AroonDown, '-r');
-hold on
-plot(70*ones(l_datalen,1));
+% figure('Name',strcat('040714',cell2mat(inputdata.commodity.name)));
+% plot(l_AroonUp, '-b');
+% hold on
+% plot(l_AroonDown, '-r');
+% hold on
+% plot(70*ones(l_datalen,1));
+% hold off
 
 
 
@@ -41,17 +42,21 @@ plot(70*ones(l_datalen,1));
 l_LongPositions = find(l_AroonUp >= 70);
 l_ShortPositions = find(l_AroonDown >=70);
 l_length1 = length(l_LongPositions);
+l_cnt = 0;
 
 for l_index = 1 : l_length1
     
-    if l_index <= length(l_LongPositions)
+    l_realindex = l_index - l_cnt;
     
-        l_temp = find(l_ShortPositions==l_LongPositions(l_index), 1);
+    if l_realindex <= length(l_LongPositions)
+    
+        l_temp = find(l_ShortPositions==l_LongPositions(l_realindex), 1);
 
         if ~isempty(l_temp)
 
-            l_LongPositions(l_index) = [];
+            l_LongPositions(l_realindex) = [];
             l_ShortPositions(l_temp) = [];
+            l_cnt = l_cnt + 1;
 
         end
         
@@ -143,76 +148,154 @@ if isequal(zeros(length(inputdata.commodity.dailyinfo.trend),1),inputdata.commod
     
 else
     
-     l_positionheld = 0;
+    l_positionheld = 0;
 
     for l_dateindex = 1 : l_datalen
-        
-        if((~isempty(find(l_LongPositions==l_dateindex,1))) && l_positionheld ~= 1 && inputdata.commodity.dailyinfo.trend(l_dateindex) == 2)
-            
-            if l_dateindex == l_datalen
-                
-                outputdata.orderlist.direction = 1;
-                outputdata.orderlist.price = 0;
-                outputdata.orderlist.name = inputdata.commodity.serialmkdata.ctname(l_dateindex);
-                l_positionheld = 1;
-            
-            else
-                
-                outputdata.record.opdate(end+1) = inputdata.commodity.serialmkdata.date(l_dateindex+1);
-                outputdata.record.opdateprice(end+1) = inputdata.commodity.serialmkdata.op(l_dateindex+1) + inputdata.commodity.serialmkdata.gap(l_dateindex+1);
-                outputdata.record.direction(end+1) = 1;
-                outputdata.record.ctname(end+1) = inputdata.commodity.serialmkdata.ctname(l_dateindex+1);
-                l_positionheld = 1;
-                
+
+        if(l_positionheld == 0)
+
+            if (~isempty(find(l_LongPositions==l_dateindex,1))) && inputdata.commodity.dailyinfo.trend(l_dateindex) == 2
+
+                if l_dateindex == l_datalen
+
+                    outputdata.orderlist.direction = 1;
+                    outputdata.orderlist.price = 0;
+                    outputdata.orderlist.name = inputdata.commodity.serialmkdata.ctname(l_dateindex);
+
+                else
+
+                    outputdata.record.opdate(end+1) = inputdata.commodity.serialmkdata.date(l_dateindex+1);
+                    outputdata.record.opdateprice(end+1) = inputdata.commodity.serialmkdata.op(l_dateindex+1) + inputdata.commodity.serialmkdata.gap(l_dateindex+1);
+                    outputdata.record.direction(end+1) = 1;
+                    outputdata.record.isclosepos(end+1) = 0;
+                    outputdata.record.ctname(end+1) = inputdata.commodity.serialmkdata.ctname(l_dateindex+1);
+                    l_positionheld = 1;
+
+                end            
+
+            elseif (~isempty(find(l_ShortPositions==l_dateindex,1))) && inputdata.commodity.dailyinfo.trend(l_dateindex) == 1
+
+                if l_dateindex == l_datalen
+
+                    outputdata.orderlist.direction = -1;
+                    outputdata.orderlist.price = 0;
+                    outputdata.orderlist.name = inputdata.commodity.serialmkdata.ctname(l_dateindex);
+
+                else
+
+                    outputdata.record.opdate(end+1) = inputdata.commodity.serialmkdata.date(l_dateindex+1);
+                    outputdata.record.opdateprice(end+1) = inputdata.commodity.serialmkdata.op(l_dateindex+1) + inputdata.commodity.serialmkdata.gap(l_dateindex+1);
+                    outputdata.record.direction(end+1) = -1;
+                    outputdata.record.isclosepos(end+1) = 0;
+                    outputdata.record.ctname(end+1) = inputdata.commodity.serialmkdata.ctname(l_dateindex+1);
+                    l_positionheld = -1;
+
+                end
+
             end
-        
-        elseif ((~isempty(find(l_ShortPositions==l_dateindex,1))) && l_positionheld ~= -1 && inputdata.commodity.dailyinfo.trend(l_dateindex) == 1)
-            
-             if l_dateindex == l_datalen
-                
-                outputdata.orderlist.direction = -1;
-                outputdata.orderlist.price = 0;
-                outputdata.orderlist.name = inputdata.commodity.serialmkdata.ctname(l_dateindex);
-                l_positionheld = -1;
-            
-            else
-                
-                outputdata.record.opdate(end+1) = inputdata.commodity.serialmkdata.date(l_dateindex+1);
-                outputdata.record.opdateprice(end+1) = inputdata.commodity.serialmkdata.op(l_dateindex+1) + inputdata.commodity.serialmkdata.gap(l_dateindex+1);
-                outputdata.record.direction(end+1) = -1;
-                outputdata.record.ctname(end+1) = inputdata.commodity.serialmkdata.ctname(l_dateindex+1);
-                l_positionheld = -1;
-                
-             end
-            
-        end
+
+        elseif l_positionheld == -1
+
+            if (~isempty(find(l_LongPositions==l_dateindex,1))) || inputdata.commodity.dailyinfo.trend(l_dateindex) == 2
+
+                if l_dateindex == l_datalen
+
+                    outputdata.orderlist.direction = 1;
+                    outputdata.orderlist.price = 0;
+                    outputdata.orderlist.name = inputdata.commodity.serialmkdata.ctname(l_dateindex);
+
+                else
+
+                    outputdata.record.cpdate(end+1) = inputdata.commodity.serialmkdata.date(l_dateindex+1);
+                    outputdata.record.cpdateprice(end+1) = inputdata.commodity.serialmkdata.op(l_dateindex+1) + inputdata.commodity.serialmkdata.gap(l_dateindex+1);
+                    outputdata.record.isclosepos(end) = 1;
+                    l_positionheld = 0;
+
+                end
+
+            end
+
+            if (~isempty(find(l_LongPositions==l_dateindex,1))) && inputdata.commodity.dailyinfo.trend(l_dateindex) == 2
+
+                if l_dateindex == l_datalen
+
+                    outputdata.orderlist.direction = 1;
+                    outputdata.orderlist.price = 0;
+                    outputdata.orderlist.name = inputdata.commodity.serialmkdata.ctname(l_dateindex);
+
+                else
+
+                    outputdata.record.opdate(end+1) = inputdata.commodity.serialmkdata.date(l_dateindex+1);
+                    outputdata.record.opdateprice(end+1) = inputdata.commodity.serialmkdata.op(l_dateindex+1) + inputdata.commodity.serialmkdata.gap(l_dateindex+1);
+                    outputdata.record.direction(end+1) = 1;
+                    outputdata.record.isclosepos(end+1) = 0;
+                    outputdata.record.ctname(end+1) = inputdata.commodity.serialmkdata.ctname(l_dateindex+1);
+                    l_positionheld = 1;
+
+                end
+
+            end
+
+        elseif l_positionheld == 1 
+
+            if (~isempty(find(l_ShortPositions==l_dateindex,1))) || inputdata.commodity.dailyinfo.trend(l_dateindex) == 1
+
+                if l_dateindex == l_datalen
+
+                    outputdata.orderlist.direction = -1;
+                    outputdata.orderlist.price = 0;
+                    outputdata.orderlist.name = inputdata.commodity.serialmkdata.ctname(l_dateindex);
+
+                else
+
+                    outputdata.record.cpdate(end+1) = inputdata.commodity.serialmkdata.date(l_dateindex+1);
+                    outputdata.record.cpdateprice(end+1) = inputdata.commodity.serialmkdata.op(l_dateindex+1) + inputdata.commodity.serialmkdata.gap(l_dateindex+1);
+                    outputdata.record.isclosepos(end) = 1;
+                    l_positionheld = 0;
+
+                end
+
+            end
+
+            if (~isempty(find(l_ShortPositions==l_dateindex,1))) && inputdata.commodity.dailyinfo.trend(l_dateindex) == 1
+
+                if l_dateindex == l_datalen
+
+                    outputdata.orderlist.direction = -1;
+                    outputdata.orderlist.price = 0;
+                    outputdata.orderlist.name = inputdata.commodity.serialmkdata.ctname(l_dateindex);
+
+                else
+
+                    outputdata.record.opdate(end+1) = inputdata.commodity.serialmkdata.date(l_dateindex+1);
+                    outputdata.record.opdateprice(end+1) = inputdata.commodity.serialmkdata.op(l_dateindex+1) + inputdata.commodity.serialmkdata.gap(l_dateindex+1);
+                    outputdata.record.direction(end+1) = -1;
+                    outputdata.record.isclosepos(end+1) = 0;
+                    outputdata.record.ctname(end+1) = inputdata.commodity.serialmkdata.ctname(l_dateindex+1);
+                    l_positionheld = -1;
+
+                end
+
+            end
+
+        end     
 
     end
     
-    %补充outputdata.record中的信息
-    if(length(outputdata.record.opdate)>=2)
-        
-        outputdata.record.cpdate=outputdata.record.opdate(2:end);
-        outputdata.record.cpdateprice=outputdata.record.opdateprice(2:end);
-        outputdata.record.isclosepos=ones(1,length(outputdata.record.opdateprice)-1);
-        outputdata.record.isclosepos(length(outputdata.record.opdateprice))=0;
-        outputdata.record.cpdate(length(outputdata.record.opdateprice))=inputdata.commodity.serialmkdata.date(end); 
-        outputdata.record.cpdateprice(length(outputdata.record.opdateprice))=inputdata.commodity.serialmkdata.cp(end);
-        
-    elseif(length(outputdata.record.opdate) ==1)
-        
-        outputdata.record.cpdate=inputdata.commodity.serialmkdata.date(end);
-        outputdata.record.cpdateprice=inputdata.commodity.serialmkdata.cp(end)+inputdata.commodity.serialmkdata.gap(end);
-        outputdata.record.isclosepos=0;
-        
+    if l_positionheld ~= 0
+     outputdata.record.cpdate(end+1) = inputdata.commodity.serialmkdata.date(end);
+     outputdata.record.cpdateprice(end+1) = inputdata.commodity.serialmkdata.op(end) + inputdata.commodity.serialmkdata.gap(end);
     end
     
     %填写outputdata.dailyinfo
     outputdata.dailyinfo.date=inputdata.commodity.serialmkdata.date;
     outputdata.dailyinfo.trend = inputdata.commodity.serialmkdata.date; 
-    
+
 end
 
-disp('finished');
+
+
+%disp('finished');
+
 
 end
